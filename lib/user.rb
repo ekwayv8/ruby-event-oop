21a96a75d083292f95bb69ae09ecaef78ad2ea4a
ekwayv8 <yvon.ekwalla@gmail.com>
2019-07-22 13:57:45 +0200

    :Finalisation du point 2.2

--- lib/user.rb
+++ lib/user.rb
@@ -5,16 +5,19 @@ class User
 
 	attr_accessor :email, :age
 
-	@@user_count = 0
+
+	#création de ma variable de classe stocker mes utilisateurs dans un array
 	@@all_users = []
 
+	#Méthode permettant d'initialiser la création des différents users
 	def initialize(email_to_save,age_to_save)
 		@email = email_to_save
-		@age = age_to_save
-		@@user_count = @@user_count + 1
+		@age = age_to_save.to_i
+		@@all_users << self
 
 	end
 
+	#Methode de classe qui retourne le array all_users
 	def self.all
 		return @@all_users	
 	end
