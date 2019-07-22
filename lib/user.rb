7644f5bf049c5872b108e9cc32910f296cf1075b
ekwayv8 <yvon.ekwalla@gmail.com>
2019-07-22 13:38:13 +0200

    Création des derniers fichiers et début création de la classe User

--- lib/user.rb
+++ lib/user.rb
@@ -1,3 +1,24 @@
+require "PRY"
+
+
 class User
 
+	attr_accessor :email, :age
+
+	@@user_count = 0
+	@@all_users = []
+
+	def initialize(email_to_save,age_to_save)
+		@email = email_to_save
+		@age = age_to_save
+		@@user_count = @@user_count + 1
+
+	end
+
+	def self.all
+		return @@all_users	
+	end
+
 end
+
+binding.pry
